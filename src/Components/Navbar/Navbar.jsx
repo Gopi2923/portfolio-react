e630b666159473103d7d0f9dc692e30133729940
Gopi2923 <dgopidhikshan@gmail.com>
2024-04-07 21:27:51 +0530

    Format changes.

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import './Navbar.css'
-import logo from '../../assets/logo.svg'
+import React from "react";
+import "./Navbar.css";
+import logo from "../../assets/logo.svg";
 
 const Navbar = () => {
   return (
-    <div className='navbar'>
+    <div className="navbar">
       <img src={logo} alt="" />
-      <ul className='nav-menu'>
+      <ul className="nav-menu">
         <li>Home</li>
         <li>About Me</li>
         <li>Services</li>
@@ -15,7 +15,7 @@ const Navbar = () => {
       </ul>
       <div className="nav-connect">Connect With Me</div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
