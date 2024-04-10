473b0eac416aa18b9539148c8623a435b700f9a1
Gopi2923 <dgopidhikshan@gmail.com>
2024-04-10 18:51:40 +0530

    Format changes

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div className='about'>
+    <div className="about">
       <div className="about-title">
         <h1>About me</h1>
-        
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default About;
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,17 +1,26 @@
 import React from "react";
-import './Hero.css'
-import profile_img from '../../assets/profile_img.svg'
+import "./Hero.css";
+import profile_img from "../../assets/profile_img.svg";
 
 const Hero = () => {
-  return <div className="hero">
-       <img src={profile_img} alt="" />
-       <h1><span>I'm Gopi D,</span> frontend developer based in India.</h1>
-       <p>Results-Driven Frontend Developer with 2 Years of Experience | Expertise in Crafting Responsive and User-Friendly Web Interfaces | Proficient in HTML, CSS, JavaScript, React, Redux and Modern Frontend Frameworks | Strong Problem-Solving Skills.</p>
-       <div className="hero-action">
+  return (
+    <div className="hero">
+      <img src={profile_img} alt="" />
+      <h1>
+        <span>I'm Gopi D,</span> frontend developer based in India.
+      </h1>
+      <p>
+        Results-Driven Frontend Developer with 2 Years of Experience | Expertise
+        in Crafting Responsive and User-Friendly Web Interfaces | Proficient in
+        HTML, CSS, JavaScript, React, Redux and Modern Frontend Frameworks |
+        Strong Problem-Solving Skills.
+      </p>
+      <div className="hero-action">
         <div className="hero-connect">Connect with me</div>
         <div className="hero-resume">My resume</div>
-       </div>
-  </div>;
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
