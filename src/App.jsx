b41ec99bf8bd3a3314c1398766e77ee9b68a6821
Gopi2923 <dgopidhikshan@gmail.com>
2024-04-11 23:23:41 +0530

    Update.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import Hero from "./Components/Hero/Hero";
-import About from "./Components/About/About"
+import About from "./Components/About/About";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
-    <About />
+      <About />
     </div>
   );
 };
