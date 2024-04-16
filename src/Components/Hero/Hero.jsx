ea7eceb8ab4443a3de28563eab5c33ed701b034f
Gopi2923 <dgopidhikshan@gmail.com>
2024-04-16 18:31:56 +0530

    Added navbar header navigation.

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./Hero.css";
 import profile_img from "../../assets/profile_img.svg";
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+
 
 const Hero = () => {
   return (
-    <div className="hero">
+    <div id="home" className="hero">
       <img src={profile_img} alt="" />
       <h1>
         <span>I'm Gopi D,</span> frontend developer based in India.
@@ -16,7 +18,7 @@ const Hero = () => {
         Strong Problem-Solving Skills.
       </p>
       <div className="hero-action">
-        <div className="hero-connect">Connect with me</div>
+        <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect With Me</AnchorLink></div>
         <div className="hero-resume">My resume</div>
       </div>
     </div>
