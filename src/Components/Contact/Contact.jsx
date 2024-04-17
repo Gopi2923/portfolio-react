2decd39df0e05578810b14172e1e39489414403f
Gopi2923 <dgopidhikshan@gmail.com>
2024-04-17 09:54:39 +0530

    Added email notification.

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -6,6 +6,31 @@ import location_icon from '../../assets/location_icon.svg'
 import call_icon from '../../assets/call_icon.svg'
 
 const Contact = () => {
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+    
+        formData.append("access_key", "4d0619a4-1013-454a-ad61-adb7215503e9");
+    
+        const object = Object.fromEntries(formData);
+        const json = JSON.stringify(object);
+    
+        const res = await fetch("https://api.web3forms.com/submit", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+          },
+          body: json
+        }).then((res) => res.json());
+    
+        if (res.success) {
+          alert(res.message);
+        }
+      };
+
+      
   return (
     <div id='contact' className='contact'>
       <div className="contact-title">
@@ -29,7 +54,7 @@ const Contact = () => {
             </div>
         </div>
 
-        <form className="contact-right">
+        <form onSubmit={onSubmit} className="contact-right">
             <label htmlFor="">Your Name</label>
             <input type="text"  placeholder='Enter your name' name='name'/>
             <label htmlFor="">Your Email</label>
